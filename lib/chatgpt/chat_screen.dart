1fa9eb66f1c8e8aa597fd36032e02fd6c314c672
Mohammed Alamer <mohammed.alamer@edu.fh-joanneum.at>
2023-08-04 19:54:31 +0200

    worked on the UI of the Chatscreen

--- lib/chatgpt/chat_screen.dart
+++ lib/chatgpt/chat_screen.dart
@@ -1,124 +1,134 @@
+import 'package:chatXpress/assets/colors/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  const ChatScreen({Key? key}) : super(key: key);
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final TextEditingController controller = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
   final List<ChatMessage> _messages = [];
-  List<Messages> messages = [];
+  final List<Messages> messages = [];
 
-  // create an instance of the OpenAI api
   final openAI = OpenAI.instance.build(
-      token: "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",
-      baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 10)),
-      enableLog: true);
+    token: "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",
+    baseOption: HttpSetup(receiveTimeout: const Duration(seconds: 10)),
+    enableLog: true,
+  );
 
-  void sendPrompt(String prompt) async {
-
-    // add the user message to the list
-    // @role can be used to display the UI depending the sender role
+  void _sendMessage(String prompt) async {
     messages.add(Messages(
       role: Role.user,
       content: prompt,
     ));
 
-    // create a request -> in the future can be replaced with a stream
-    // advanatage is that the stream is stays opened until a specific case
-    // ex: the user leaves the chat
-    final request = ChatCompleteText(messages: messages, maxToken: 200,
-        model: GptTurboChatModel());
+    ChatMessage messageWidget = ChatMessage(text: prompt, sender: "user");
+    _messages.add(messageWidget);
+    setState(() {});
 
-    // response
+    final request = ChatCompleteText(
+      messages: messages,
+      maxToken: 200,
+      model: GptTurboChatModel(),
+    );
+
     final response = await openAI.onChatCompletion(request: request);
 
-    // get the message object from the response
-    var message = response!.choices.last.message;
+    var message = response?.choices.isNotEmpty == true
+        ? response!.choices.last.message
+        : null;
 
-    // create a ui element from the response
-    _messages.add(ChatMessage(
-             text: message!.content, sender: "ChatGpt"));
-
-
-
-    // without this, the context would be much harder for the LLM to
-    // understand, since the messages list would only contain the questions.
-    // ***TIP***: remove this statement and compare the answers you would get
-    // from the LLM, so you have a better understanding of the functionality.
-    messages.add(Messages(role: Role.assistant,
-    content: message.content));
-
-    setState(() {});
+    if (message != null) {
+      setState(() {
+        _messages.add(
+            ChatMessage(
+              text: message.content ??
+                  "Something went wrong, please try again later",
+              sender: "ChatGpt",
+            ));
+      });
+    }
   }
 
-  void _sendMessage() {
-    ChatMessage message = ChatMessage(text: controller.text, sender: "user");
-    setState(() {
-      sendPrompt(controller.text);
-      _messages.add(message);
-      controller.clear();
-    });
+  void _handleSendMessage() {
+    String prompt = _controller.text.trim();
+    if (prompt.isNotEmpty) {
+      _sendMessage(prompt);
+      _controller.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("TestScreen"),
+        title: const Text("Chat Screen"),
       ),
       body: Column(
         children: [
-          Flexible(
-              child: ListView.builder(
-                  itemCount: _messages.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: ChatMessage(
-                          text: _messages[index].text,
-                          sender: _messages[index].sender),
-                    );
-                  })),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: ChatMessage(
+                    text: _messages[index].text,
+                    sender: _messages[index].sender,
+                  ),
+                );
+              },
+            ),
+          ),
           Container(
-            decoration: const BoxDecoration(color: Colors.white),
-            child: _buildTextComposer(_sendMessage, controller),
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(0, -2),
+                  blurRadius: 4,
+                ),
+              ],
+            ),
+            child: _buildTextComposer(),
           )
         ],
       ),
     );
   }
-}
 
-Widget _buildTextComposer(
-    Function sendMessage, TextEditingController textEditingController) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 16.0),
-    child: Row(
+  Widget _buildTextComposer() {
+    return Row(
       children: [
         Expanded(
           child: TextField(
-            controller: textEditingController,
-            decoration:
-                const InputDecoration(isCollapsed: true, hintText: "Send a message"),
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: "Send a message",
+              border: InputBorder.none,
+            ),
+            onSubmitted: (value) => _handleSendMessage(),
           ),
         ),
         IconButton(
-            onPressed: () {
-              sendMessage();
-            },
-            icon: const Icon(Icons.send))
+          onPressed: () => _handleSendMessage(),
+          icon: const Icon(Icons.send),
+          color: MyColors.greenDefaultColor,
+        )
       ],
-    ),
-  );
+    );
+  }
 }
 
 class ChatMessage extends StatelessWidget {
-  const ChatMessage({super.key, required this.text, required this.sender});
+  const ChatMessage({Key? key, required this.text, required this.sender})
+      : super(key: key);
 
   final String text;
   final String sender;
@@ -126,19 +136,21 @@ class ChatMessage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisAlignment: sender == "ChatGpt" ? MainAxisAlignment.start : MainAxisAlignment.end,
       children: [
-        Container(
-          margin: const EdgeInsets.only(right: 16.0),
-          child: CircleAvatar(child: Text(sender[0])),
+        CircleAvatar(child: Text(sender[0])),
+        const SizedBox(width: 8),
+        Flexible(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: sender == "ChatGpt" ? MyColors.greenDefaultColor : Colors.grey[200],
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(text),
+          ),
         ),
-        Expanded(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(text),
-            Container(margin: const EdgeInsets.only(top: 5.0))
-          ],
-        ))
       ],
     );
   }
