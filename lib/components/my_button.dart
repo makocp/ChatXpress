1f9472f373fb864983b78e2d90f8021b504235ec
Mohammed Alamer <mohammed.alamer@edu.fh-joanneum.at>
2023-08-06 13:09:45 +0200

    changed the mybutton widget

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -2,30 +2,30 @@ import 'package:chatXpress/assets/colors/my_colors.dart';
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-
-  final Function()? onTap;
+  final VoidCallback? onTap; // Changed Function()? to VoidCallback?
   final String buttonText;
-  const MyButton({super.key, required this.onTap, required this.buttonText});
+
+  const MyButton({Key? key, required this.onTap, required this.buttonText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
+    return ElevatedButton(
+      onPressed: onTap, // Changed onTap to onPressed
+      style: ElevatedButton.styleFrom(
+        backgroundColor: MyColors.greenDefaultColor,
         padding: const EdgeInsets.all(15),
-        decoration: const BoxDecoration(
-            // color: Colors.black,
-            color: MyColors.greenDefaultColorDark,
-            borderRadius: BorderRadius.all(Radius.circular(10.0))),
-        child: Center(
-            child: Text(
-          buttonText,
-          style: const TextStyle(
-            color: Colors.white,
-            // fontWeight: FontWeight.bold,
-            fontSize: 18,
-          ),
-        )),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+      ),
+      child: Text(
+        buttonText,
+        style: const TextStyle(
+          color: Colors.white,
+          // fontWeight: FontWeight.bold,
+          fontSize: 18,
+        ),
       ),
     );
   }
