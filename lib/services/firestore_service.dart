e847a48aff0cb68364017bc255a719ec10153f8a
makocp <mako.codeproducer@gmail.com>
2023-08-14 10:49:52 +0200

    create user after sign up added

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,7 +1,15 @@
+import 'package:chatXpress/services/auth_service.dart';
+import 'package:chatXpress/services_provider/service_container.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class FirestoreService {
-  
-  void savePromptToDb(String prompt){
+  final db = FirebaseFirestore.instance;
+  final authService = serviceContainer<AuthService>();
 
+  Future<void> saveUserToDb(String email) async {
+    return await db
+        .collection('users')
+        .doc(authService.firebaseAuthInstance.currentUser?.uid.toString())
+        .set({"email": email});
   }
-
 }
