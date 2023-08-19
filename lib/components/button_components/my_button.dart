49e8e3be9f8f267909370229cacdb83705795a2d
makocp <mako.codeproducer@gmail.com>
2023-08-19 02:36:53 +0200

    loading button combined to one component

--- lib/components/button_components/my_button.dart
+++ lib/components/button_components/my_button.dart
@@ -4,8 +4,13 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final VoidCallback? onPressed;
   final String buttonText;
+  final bool isLoading;
 
-  const MyButton({Key? key, required this.onPressed, required this.buttonText})
+  const MyButton(
+      {Key? key,
+      required this.onPressed,
+      required this.buttonText,
+      required this.isLoading})
       : super(key: key);
 
   @override
@@ -21,14 +26,27 @@ class MyButton extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(10.0)),
           ),
         ),
-        child: Text(
-          buttonText,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 18,
-          ),
-        ),
+        child: showButtonContent(),
       ),
     );
+  }
+
+  showButtonContent() {
+    return isLoading
+        ? const SizedBox(
+            height: 20,
+            width: 20,
+            child: Center(
+                child: CircularProgressIndicator(
+              color: Colors.white,
+            )),
+          )
+        : Text(
+            buttonText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          );
   }
 }
