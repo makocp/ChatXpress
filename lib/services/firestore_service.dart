6a7c44176ebc0edb6d711106b0f9af56753867de
Mohammed Alamer <mohammed.alamer@edu.fh-joanneum.at>
2023-08-26 21:51:33 +0200

    added a check to see if a user already exists

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -9,12 +9,29 @@ class FirestoreService {
   // Creates a new user, if it does not exist.
   // If it DOES exist it updates the sent fields, without deleting the others (merge true).
   // This merge option is only, if something failed during account creation -> user gets created after new sign in, just in case.
-  Future<void> setUser(String email,String username) async {
-    return await db
+  Future<void> setUser(String email, String username) async {
+    // Check if the user with the given email already exists
+    QuerySnapshot userSnapshot = await db
         .collection('users')
-        .doc(authService.firebaseAuthInstance.currentUser?.uid.toString())
-        .set({"email": email, "username": username}, SetOptions(merge: true));
+        .where('email', isEqualTo: email)
+        .get();
+
+    // If a user with the given email exists, update their information
+    if (userSnapshot.docs.isNotEmpty) {
+      String userId = userSnapshot.docs[0].id;
+      await db.collection('users').doc(userId).set(
+        {"username": username},
+        SetOptions(merge: true),
+      );
+    } else {
+      // If no user with the given email exists, create a new user document
+      await db.collection('users').add({
+        "email": email,
+        "username": username.split(" ")[0],
+      });
+    }
   }
+
 
   Future<void> deleteUser() async {}
 
