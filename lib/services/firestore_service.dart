81a7d79ca608367f28553f58d961e2c372bdd8b0
Mohammed Alamer <mohammed.alamer@edu.fh-joanneum.at>
2023-08-26 22:16:19 +0200

    resolved merge conflict

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,3 +1,4 @@
+import 'package:chatXpress/models/message_model.dart';
 import 'package:chatXpress/services/auth_service.dart';
 import 'package:chatXpress/services_provider/service_container.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,24 +7,20 @@ class FirestoreService {
   final db = FirebaseFirestore.instance;
   final authService = serviceContainer<AuthService>();
 
+  currentUserID() =>
+      authService.firebaseAuthInstance.currentUser?.uid.toString();
+
+  // CREATE / UPDATE
   // Creates a new user, if it does not exist.
   // If it DOES exist it updates the sent fields, without deleting the others (merge true).
   // This merge option is only, if something failed during account creation -> user gets created after new sign in, just in case.
   Future<void> setUser(String email, String username) async {
     // Check if the user with the given email already exists
-    QuerySnapshot userSnapshot = await db
-        .collection('users')
-        .where('email', isEqualTo: email)
-        .get();
+    QuerySnapshot userSnapshot =
+        await db.collection('users').where('email', isEqualTo: email).get();
 
     // If a user with the given email exists, update their information
-    if (userSnapshot.docs.isNotEmpty) {
-      String userId = userSnapshot.docs[0].id;
-      await db.collection('users').doc(userId).set(
-        {"username": username},
-        SetOptions(merge: true),
-      );
-    } else {
+    if (userSnapshot.docs.isEmpty) {
       // If no user with the given email exists, create a new user document
       await db.collection('users').add({
         "email": email,
@@ -32,8 +29,51 @@ class FirestoreService {
     }
   }
 
+  // CREATE
+  // to add a message to the db with chatId as mapping to a chat.
+  addMessage(MessageModel message) async {
+    await db.collection('messages').add(message.mapToDB());
+  }
 
-  Future<void> deleteUser() async {}
+  // CREATE / UPDATE
+  // to set a chat to the db.
+  // gets updated, if document exists, but field changes (e.g. title) -> Setoptions merge: true (same as update, but creates document, if does not exist -> update would throw error).
+  setChat(String chatId, String title) async {
+    await db.collection('chats').doc(chatId).set(
+        {'title': title, 'userId': currentUserID(), 'date': DateTime.now()},
+        SetOptions(merge: true));
+  }
 
-  Future<void> getUser() async {}
+  // READ
+  Future<QuerySnapshot<Map<String, dynamic>>> getCurrentUserChats() async {
+    return await db
+        .collection('chats')
+        .orderBy('date', descending: true)
+        .where('userId', isEqualTo: currentUserID())
+        // GetOptions -> by default ServerAndCache (Loads from Server, but from Cache, if Server not available).
+        .get();
+  }
+
+  // READ
+  Future<QuerySnapshot<Map<String, dynamic>>> getChatMessages(
+      String chatId) async {
+    return await db
+        .collection('messages')
+        .orderBy('date', descending: false)
+        .where('chatId', isEqualTo: chatId)
+        .get();
+  }
+
+  // DELETE
+  Future deleteChats() async {
+    await db
+        .collection('chats')
+        .where('userId', isEqualTo: currentUserID())
+        .get()
+        .then((value) {
+      for (var doc in value.docs) {
+        doc.reference.delete();
+      }
+    });
+  }
 }
