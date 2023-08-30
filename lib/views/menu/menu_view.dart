cd1b66886f65c46c5fb5c6af060ca9a70f0f407c
makocp <mako.codeproducer@gmail.com>
2023-08-31 01:56:19 +0200

    removed  temporary changepassword

--- lib/views/menu/menu_view.dart
+++ lib/views/menu/menu_view.dart
@@ -1,9 +1,6 @@
 import 'package:chatXpress/assets/colors/my_colors.dart';
 import 'package:chatXpress/assets/snackbars/snackbars.dart';
 import 'package:chatXpress/assets/strings/my_strings.dart';
-import 'package:chatXpress/components/box_components/my_infobox.dart';
-import 'package:chatXpress/components/button_components/my_button.dart';
-import 'package:chatXpress/components/textfield_components/my_passwordfield.dart';
 import 'package:chatXpress/services_provider/service_container.dart';
 import 'package:chatXpress/views/chat/chat_viewmodel.dart';
 import 'package:chatXpress/views/menu/menu_viewmodel.dart';
@@ -13,9 +10,6 @@ import 'package:get_it_mixin/get_it_mixin.dart';
 class MenuView extends StatelessWidget with GetItMixin {
   MenuView({super.key});
 
-  final TextEditingController _newPasswordController = TextEditingController();
-  final TextEditingController _newPasswordConfirmationController =
-      TextEditingController();
   final _menuViewmodel = serviceContainer<MenuViewmodel>();
   final _chatViewmodel = serviceContainer<ChatViewmodel>();
 
@@ -24,10 +18,6 @@ class MenuView extends StatelessWidget with GetItMixin {
     bool isLoadingChats = watchOnly((ChatViewmodel vm) => vm.isLoadingChats);
     bool isLoadingRequestResponse =
         watchOnly((ChatViewmodel vm) => vm.isLoading);
-    bool isLoadingChangePassword =
-        watchOnly((MenuViewmodel vm) => vm.isLoadingChangePassword);
-    String changePasswordMessage =
-        watchOnly((MenuViewmodel vm) => vm.changePasswordMessage);
     return Drawer(
       backgroundColor: MyColors.greyMenuBackground,
       child: SafeArea(
@@ -37,8 +27,7 @@ class MenuView extends StatelessWidget with GetItMixin {
             children: [
               showTopSection(context, isLoadingRequestResponse),
               buildUserChats(isLoadingChats, isLoadingRequestResponse),
-              showBottomSection(context, isLoadingRequestResponse,
-                  isLoadingChangePassword, changePasswordMessage),
+              showBottomSection(context, isLoadingRequestResponse),
             ],
           ),
         ),
@@ -104,14 +93,12 @@ class MenuView extends StatelessWidget with GetItMixin {
     );
   }
 
-  Column showBottomSection(BuildContext context, bool isLoadingRequestResponse,
-      bool isLoadingChangePassword, String changePasswordMessage) {
+  Column showBottomSection(
+      BuildContext context, bool isLoadingRequestResponse) {
     return Column(
       children: [
         const Divider(color: Colors.white),
         showDeleteHistoryButton(context, isLoadingRequestResponse),
-        showChangePasswordButton(context, isLoadingChangePassword,
-            changePasswordMessage, isLoadingRequestResponse),
         showLogoutButton(context),
       ],
     );
@@ -136,30 +123,6 @@ class MenuView extends StatelessWidget with GetItMixin {
                   })
                 })
       },
-    );
-  }
-
-  ListTile showChangePasswordButton(
-      BuildContext context,
-      bool isLoadingChangePassword,
-      String changePasswordMessage,
-      bool isLoadingRequestResponse) {
-    return ListTile(
-      leading: const Icon(
-        Icons.lock_reset_outlined,
-        color: Colors.white,
-      ),
-      title: const Text(MyStrings.changePassword,
-          style: TextStyle(color: Colors.white)),
-      onTap: isLoadingRequestResponse
-          ? () {
-              Navigator.pop(context);
-              MySnackBars.showSnackBar(context, MySnackBars.ongoingRequest);
-            }
-          : () {
-              showModalDialog(context, _menuViewmodel, isLoadingChangePassword,
-                  changePasswordMessage);
-            },
     );
   }
 
@@ -253,110 +216,6 @@ class MenuView extends StatelessWidget with GetItMixin {
       builder: (BuildContext context) {
         return alert;
       },
-    );
-  }
-
-  void showModalDialog(BuildContext context, MenuViewmodel menuViewModel,
-      bool isLoadingChangePassword, String changePasswordMessage) {
-    showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (context) => _buildBottomSheetContent(context, menuViewModel,
-          isLoadingChangePassword, changePasswordMessage),
-    );
-  }
-
-  Widget _buildHeader() {
-    return Padding(
-      padding: const EdgeInsets.all(30),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(30),
-        child: Container(
-          height: 10,
-          width: 100,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildBottomSheetContent(
-      BuildContext context,
-      MenuViewmodel menuViewModel,
-      bool isLoadingChangePassword,
-      String changePasswordMessage) {
-    return Container(
-      color: const Color(0xff202123),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 35),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            _buildHeader(),
-            _buildTitle(),
-            const SizedBox(
-              height: 25,
-            ),
-            _buildPasswordField(
-              controller: _newPasswordController,
-              labelText: MyStrings.enterNewPassword,
-            ),
-            changePasswordMessage.isNotEmpty
-                ? MyInfoBox(message: changePasswordMessage, isError: true)
-                : const SizedBox(height: 25),
-            _buildPasswordField(
-              controller: _newPasswordConfirmationController,
-              labelText: MyStrings.repeatNewPassword,
-            ),
-            _buildChangePasswordButton(menuViewModel, isLoadingChangePassword),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPasswordField({
-    required TextEditingController controller,
-    required String labelText,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-      child: MyPasswordfield(
-        controller: controller,
-        labelText: labelText,
-        isError: false,
-      ),
-    );
-  }
-
-  Widget _buildTitle() {
-    return const Padding(
-      padding: EdgeInsets.fromLTRB(15, 30, 15, 8),
-      child: Text(
-        MyStrings.changePassword,
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildChangePasswordButton(
-      MenuViewmodel menuViewModel, bool isLoadingChangePassword) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(15, 30, 15, 8),
-      child: MyButton(
-        onPressed: () {
-          menuViewModel.updatePassword(
-            _newPasswordController.text.trim(),
-            _newPasswordConfirmationController.text.trim(),
-          );
-        },
-        buttonText: MyStrings.buttonChangePassword,
-        isLoading: isLoadingChangePassword,
-      ),
     );
   }
 }
--- lib/views/menu/menu_viewmodel.dart
+++ lib/views/menu/menu_viewmodel.dart
@@ -1,10 +1,8 @@
 import 'dart:async';
 
-import 'package:chatXpress/assets/strings/my_strings.dart';
 import 'package:chatXpress/services/auth_service.dart';
 import 'package:chatXpress/services/firestore_service.dart';
 import 'package:chatXpress/views/chat/chat_viewmodel.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import '../../services_provider/service_container.dart';
 
@@ -12,17 +10,6 @@ class MenuViewmodel extends ChangeNotifier {
   final authService = serviceContainer<AuthService>();
   final firestoreService = serviceContainer<FirestoreService>();
   final chatViewModel = serviceContainer<ChatViewmodel>();
-
-  bool _isLoadingChangePassword = false;
-  bool get isLoadingChangePassword => _isLoadingChangePassword;
-
-  String _changePasswordMessage = '';
-  String get changePasswordMessage => _changePasswordMessage;
-
-  setLoadingChangePasswordState(bool isLoadingChangePassword) {
-    _isLoadingChangePassword = isLoadingChangePassword;
-    notifyListeners();
-  }
 
   void openNewChat() {
     chatViewModel.setDefaultChatState();
@@ -37,45 +24,6 @@ class MenuViewmodel extends ChangeNotifier {
     firestoreService.deleteChats();
     chatViewModel.deleteUserchatsState();
     chatViewModel.setDefaultChatState();
-  }
-
-  updatePassword(String newPassword, String newConfirmationPassword) async {
-    setLoadingChangePasswordState(true);
-    // Clientside validation
-    if (validatedPassword(newPassword, newConfirmationPassword)) {
-      try {
-        await authService.updatePassword(newPassword);
-        // Serverside Validation, catches error.
-      } on FirebaseAuthException catch (error) {
-        switch (error.code) {
-          case 'weak-password':
-            _changePasswordMessage = MyStrings.validationPasswordWeak;
-            break;
-          case 'requires-recent-login':
-            _changePasswordMessage = MyStrings.validationReauthentication;
-            break;
-        }
-      }
-    }
-    setLoadingChangePasswordState(false);
-  }
-
-  // return true if password is valid.
-  // otherwise message string get set for UI state.
-  bool validatedPassword(String newPassword, String newConfirmationPassword) {
-    if (newPassword.length < 8) {
-      _changePasswordMessage = MyStrings.validationPasswordLength;
-      return false;
-    }
-    if (newPassword.contains(' ')) {
-      _changePasswordMessage = MyStrings.validationPasswordSpaces;
-      return false;
-    }
-    if (newPassword != newConfirmationPassword) {
-      _changePasswordMessage = MyStrings.validationPasswordMatch;
-      return false;
-    }
-    return true;
   }
 
   Future<void> logOut() async {
